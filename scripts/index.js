e9a4de50c28d48113e1a1f0c14f4bc03b9183f1d
Gözde Hisarcıklılar <erdemlihsr@gmail.com>
2021-04-08 16:20:27 +0300

    feature: add responsive modal window

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,10 @@ let editButton = document.querySelector('.profile__button_type_edit');
 let modal = document.querySelector('.modal');
 let closeButton = document.querySelector('.form-profile__close-icon');
 
-
-editButton.addEventListener('click', function() {
-    modal.classList.add('modal_opened');
-}
-);
-
-
-closeButton.addEventListener('click', function() {
+function toggleModal() {
     modal.classList.toggle('modal_opened');
 }
-);
+editButton.addEventListener('click', toggleModal);
+
+
+closeButton.addEventListener('click', toggleModal);
