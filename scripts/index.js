56c723df908d7e40b9af36d6a687f2c14cf51b97
Gözde Hisarcıklılar <erdemlihsr@gmail.com>
2021-04-09 02:24:16 +0300

    feat: add bem folders

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,28 @@
-let editButton = document.querySelector('.profile__button_type_edit');
 let modal = document.querySelector('.modal');
-let closeButton = document.querySelector('.form-profile__close-icon');
 let formElement = document.querySelector('.form-profile');
+let editButton = document.querySelector('.profile__button_type_edit');
+let closeButton = document.querySelector('.form-profile__close-icon');
 
-function toggleModal() {
-    modal.classList.toggle('modal_opened');
-}
+let nameInput = document.querySelector('.form-profile__item_el_name');
+let subtitleInput = document.querySelector('.form-profile__item_el_subtitle');
+let profileName = document.querySelector('.profile__title-name');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
-function FormSave(event) {
+console.log(editButton);
+formElement.addEventListener('submit', function(event) {
     event.preventDefault();
-    
-    let nameInput = document.querySelector('.form-profile__item_el_name');
-    let subtitleInput = document.querySelector('.form-profile__item_el_subtitle');
-
-    let profileName = document.querySelector('.profile__title-name');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
     
     profileName.textContent = nameInput.value;
     profileSubtitle.textContent = subtitleInput.value;
 
     toggleModal();
-};
-editButton.addEventListener('click', toggleModal);
+   }
+);
 
+function toggleModal() {
+    modal.classList.toggle('modal_opened');
+}
+
+editButton.addEventListener('click', toggleModal);
 
 closeButton.addEventListener('click', toggleModal);
