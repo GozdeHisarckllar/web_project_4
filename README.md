d2bcdd17192cc0ce78c6d0b8e5554f8a5bba34b1
Gözde Hisarcıklılar <erdemlihsr@gmail.com>
2021-04-11 00:44:57 +0300

    feature: add like buttons

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 **Description**
 
-* The page contains HTML, CSS and JavaScript componenets. It is responsive to desktop and mobile screen sizes. The page includes photo cards, a modal window containing form elements and SVG components such as buttons and a logo.  In addition, while the modal/popup window allows the user to change the profile text content, photo cards include customized like buttons/checkboxes which allow the user to like photos. The layout consists of flexbox and grid technologies. Also, the project follows the BEM naming convention and file structure.
+* The page contains HTML, CSS and JavaScript componenets. It is responsive to desktop and mobile screen sizes. The page includes photo cards, a modal window containing form elements and SVG components such as buttons and a logo.  In addition, while the modal/popup window allows the user to change the profile text content, photo cards include customized like buttons which allow the user to like photos. The layout consists of flexbox and grid technologies. Also, the project follows the BEM naming convention and file structure.
 
 #### Technologies;
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,45 @@
 let modal = document.querySelector('.modal');
 let formElement = document.querySelector('.form-profile');
 let editButton = document.querySelector('.profile__button_type_edit');
-let closeButton = document.querySelector('.form-profile__close-icon');
+let closeButton = document.querySelector('.form-profile__close-btn');
 
 let nameInput = document.querySelector('.form-profile__item_el_name');
 let subtitleInput = document.querySelector('.form-profile__item_el_subtitle');
 let profileName = document.querySelector('.profile__title-name');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
+let heartButton = document.querySelectorAll('.cards__heart-btn');
 
-formElement.addEventListener('submit', function(event) {
+
+for (let i = 0; i < heartButton.length; i++) {
+    heartButton[i].addEventListener('click', function() {
+        heartButton[i].classList.toggle("cards__heart-btn_active");
+        heartButton[i].classList.toggle('like-hover');
+    }
+  );
+}
+
+function formSubmit(event) {
     event.preventDefault();
     
     profileName.textContent = nameInput.value;
     profileSubtitle.textContent = subtitleInput.value;
 
     toggleModal();
-   }
-);
+}
 
 function toggleModal() {
     modal.classList.toggle('modal_opened');
 }
 
-editButton.addEventListener('click', function(){
+function filledInputOpened() {
     nameInput.value = profileName.textContent;
     subtitleInput.value = profileSubtitle.textContent;
     toggleModal();
-});
+}
+
+formElement.addEventListener('submit', formSubmit);
+
+editButton.addEventListener('click', filledInputOpened);
 
 closeButton.addEventListener('click', toggleModal);
