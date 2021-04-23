5d6785536b57ef0d394b8d7c50b681c83767e141
Gözde Hisarcıklılar <erdemlihsr@gmail.com>
2021-04-23 22:51:34 +0300

    feature: add functionalities enable user to like, remove, create photo cards

--- README.md
+++ README.md
@@ -3,18 +3,20 @@
 ### Overview
 
 * Intro
-* Description
+* Description and functionality
 * Technologies
 
 **Intro**
 
-* A front-end project for the Sprint 4 in the [Practicum by Yandex](https://practicum.yandex.com/) curriculum.
+* A front-end project for the Sprint 4 & Sprint 5 in the [Practicum by Yandex](https://practicum.yandex.com/) curriculum.
 
-* An interactive page where users can add, remove, or like photos about various travel destinations. 
+* An interactive page/frond-end code for the photo app which enables a user to add or remove customized photo cards, view and like photos about various travel destinations.
 
-**Description**
+**Description and Functionality**
 
-* The page contains HTML, CSS and JavaScript componenets. It is responsive to desktop and mobile screen sizes. The page includes photo cards, a modal window containing form elements and SVG components such as buttons and a logo.  In addition, while the modal/popup window allows the user to change the profile text content, photo cards include customized like buttons which allow the user to like photos. The layout consists of flexbox and grid technologies. Also, the project follows the BEM naming convention and file structure.
+* The page contains HTML, CSS and JavaScript componenets. It is responsive to desktop and mobile screen sizes. The layout consists of flexbox and grid technologies. Also, the project follows the BEM naming convention and file structure.
+
+* The page includes photo cards which a user can add, like or remove and modal windows containing form elements which allows the user to edit the profile text content, lets the user view photos in detail or enables the user to create customized photo cards. These functionalities have been created via JavaScript and DOM methods.
 
 #### Technologies;
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,158 @@
-let modal = document.querySelector('.modal');
-let formElement = document.querySelector('.form-profile');
-let editButton = document.querySelector('.profile__button_type_edit');
-let closeButton = document.querySelector('.modal__close-btn');
+const editProfileModal = document.querySelector('.modal_type_edit-profile');
+const formProfileElement = document.querySelector('.form-profile_type_profile-info');
+const editButton = document.querySelector('.profile__button_type_edit');
+const editProfileCloseButton = editProfileModal.querySelector('.modal__close-btn');
 
-let nameInput = document.querySelector('.form-profile__item_el_name');
-let subtitleInput = document.querySelector('.form-profile__item_el_subtitle');
-let profileName = document.querySelector('.profile__title-name');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const addCardModal = document.querySelector('.modal_type_add-card');
+const formNewCardElement = document.querySelector('.form-profile_type_new-card-info');
+const addCardButton = document.querySelector('.profile__button_type_add');
+const addCardCloseButton = addCardModal.querySelector('.modal__close-btn');
+const profileCardTitle = document.querySelector('.form-profile__item_el_card-title');
+const profileCardLink = document.querySelector('.form-profile__item_el_card-link');
 
-function formSubmit(event) {
+const imageDetailModal = document.querySelector('.modal_type_image-detail');
+const imageDetailCloseButton = imageDetailModal.querySelector('.modal__close-btn');
+
+const nameInput = document.querySelector('.form-profile__item_el_name');
+const subtitleInput = document.querySelector('.form-profile__item_el_subtitle');
+const profileName = document.querySelector('.profile__title-name');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
+function submitForm(event) {
     event.preventDefault();
     
     profileName.textContent = nameInput.value;
     profileSubtitle.textContent = subtitleInput.value;
 
-    toggleModal();
+    toggleModal(editProfileModal);
 }
 
-function filledInputOpened() {
+function openFilledForm() {
     nameInput.value = profileName.textContent;
     subtitleInput.value = profileSubtitle.textContent;
-    toggleModal();
+    toggleModal(editProfileModal);
 }
 
-function toggleModal() {
-    modal.classList.toggle('modal_opened');
+function toggleModal(modal_type) {
+    modal_type.classList.toggle('modal_opened');
 }
 
-formElement.addEventListener('submit', formSubmit);
+formProfileElement.addEventListener('submit', submitForm);
 
-editButton.addEventListener('click', filledInputOpened);
+editButton.addEventListener('click', openFilledForm);
 
-closeButton.addEventListener('click', toggleModal);
+editProfileCloseButton.addEventListener('click', () => {
+  toggleModal(editProfileModal);
+});
+
+addCardButton.addEventListener('click', () => {
+  toggleModal(addCardModal);
+});
+
+addCardCloseButton.addEventListener('click', () => {
+  toggleModal(addCardModal);
+});
+
+imageDetailCloseButton.addEventListener('click', () => {
+  toggleModal(imageDetailModal);
+});
+
+const initialCards = [
+    {
+      name: 'Yosemite Valley',
+      link: 'https://code.s3.yandex.net/web-code/yosemite.jpg'
+    },
+    {
+      name: 'Lake Louise',
+      link: 'https://code.s3.yandex.net/web-code/lake-louise.jpg'
+    },
+    {
+      name: 'Bald Mountains',
+      link: 'https://code.s3.yandex.net/web-code/bald-mountains.jpg'
+    },
+    {
+      name: 'Latemar',
+      link: 'https://code.s3.yandex.net/web-code/latemar.jpg'
+    },
+    {
+      name: 'Vanoise National Park',
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+      name: 'Lago di Braies',
+      link: 'https://code.s3.yandex.net/web-code/lago.jpg'
+    }
+];
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.cards__list');
+
+
+function openImageDetailPreview(data) {
+  const modalImage = imageDetailModal.querySelector('.modal__image');
+  const modalImageCaption = imageDetailModal.querySelector('.modal__image-caption');
+
+  modalImage.src = data.link;
+  modalImage.alt = data.name;
+  modalImageCaption.textContent = data.name;
+    
+  toggleModal(imageDetailModal);
+}
+
+function createCard(data) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+
+    cardImage.style.backgroundImage = `url(${data.link})`;
+    cardTitle.textContent = data.name;
+
+    cardImage.addEventListener('click', () => {
+      openImageDetailPreview(data);
+    });
+
+    const cardLikeButton = cardElement.querySelector('.card__like-btn');
+    const cardRemoveButton = cardElement.querySelector('.card__remove-btn');
+
+    function toggleLikeButton() {
+      cardLikeButton.classList.toggle('card__like-btn_active');
+      cardLikeButton.classList.toggle('like-hover');
+    }
+
+    function removeCard() {
+      cardElement.remove();
+    }
+
+    cardLikeButton.addEventListener('click', toggleLikeButton);
+    
+    cardRemoveButton.addEventListener('click', removeCard);
+
+    return cardElement;
+}    
+
+function renderCard(card, container) {
+  container.prepend(card);
+}
+
+initialCards.forEach((data) => {
+  const cardElement = createCard(data);
+
+  renderCard(cardElement, cardList);
+});
+
+function addNewCard(event) {
+  event.preventDefault();
+  const newCard = {};
+  newCard.name = profileCardTitle.value;
+  newCard.link = profileCardLink.value;
+
+  renderCard(createCard(newCard), cardList);
+
+  profileCardTitle.value = "";
+  profileCardLink.value = "";
+}
+
+formNewCardElement.addEventListener('submit', (event) => {
+  toggleModal(addCardModal);
+  addNewCard(event);
+});
