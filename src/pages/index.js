e0f71dada63532588f84ad7c2e54af9b3be51d72
Gözde Hisarcıklılar <erdemlihsr@gmail.com>
2021-06-12 17:27:13 +0300

    refactor: reused some public methods and functions to remove code duplicates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,16 @@ import PopupWithImage from "../components/PopupWithImage.js";
 import Section from "../components/Section.js";
 import UserInfo from "../components/UserInfo.js";
 
-export const editProfileModal = document.querySelector('.modal_type_edit-profile');
-export const formProfileElement = document.querySelector('.form_type_profile-info');
-export const editButton = document.querySelector('.profile__button_type_edit');
-export const editSubmitButton = editProfileModal.querySelector(".form__button");
+const editProfileModal = document.querySelector('.modal_type_edit-profile');
+const formProfileElement = document.querySelector('.form_type_profile-info');
+const editButton = document.querySelector('.profile__button_type_edit');
+const editSubmitButton = editProfileModal.querySelector(".form__button");
 
-export const formNewCardElement = document.querySelector('.form_type_new-card-info');
-export const addCardButton = document.querySelector('.profile__button_type_add');
+const formNewCardElement = document.querySelector('.form_type_new-card-info');
+const addCardButton = document.querySelector('.profile__button_type_add');
 
-export const nameInput = document.querySelector('.form__item_el_name');
-export const subtitleInput = document.querySelector('.form__item_el_subtitle');
+const nameInput = document.querySelector('.form__item_el_name');
+const subtitleInput = document.querySelector('.form__item_el_subtitle');
 
 
 const profileFormValidator = new FormValidator(formSettings, formProfileElement);
